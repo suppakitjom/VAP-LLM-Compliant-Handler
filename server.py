36f635c2f3c13c61793dbc07664e3b402209c788
suppakitjom <6438232221@student.chula.ac.th>
2024-08-29 17:08:22 +0700

    combined server files

--- server.py
+++ server.py
@@ -1,15 +1,51 @@
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from langserve import add_routes
 from fastapi.middleware.cors import CORSMiddleware
+import json
 import os
 from config import LANGCHAIN_API_KEY, OPENAI_API_KEY, GEMINI_API_KEY, GOOGLE_API_KEY
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+ON_PREM = True
+
+summarizer_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            '''You are a helpful assistant that digests a complaint in Thai given into JSON format, in Thai as follow:
+    ### format
+     Summary: '...', AllegedParty: '...', Accusation: '...', Location: '...', Amount: '...'
+    
+    ### field description
+     where Summary is the concise summary of the complaint, and just explain what the alleged party is accused of, 
+     AllegedParty is the person(people) or/and organization(s) being accused (NOT THE ACCUSER), if there are multiple, separate them with comma, 
+     Accusation is the action that the perpetrator is accused of, 
+     Location is the province, district, or subdistrict, 
+     Amount is the amount of money, if there is any involved, in the format of (number) บาท for example 1,000,000 บาท
+    
+    ### additional information
+     the Summary is the only field that is required. if information is not explicitly available for any other field, just leave it as empty string
+     EVERY FIELD MUST BE FILLED IN FORMAL THAI LANGUAGE''',
+        ),
+        ("human", "{complaint}"),
+    ]
+)
+
+if ON_PREM:
+    json_llm = ChatOllama(model="gemma2",format="json",temperature=0)
+else:
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    json_llm = llm.bind(response_format={"type": "json_object"})
+
+json_parser = JsonOutputParser()
+
+summarizer_chain = summarizer_prompt | json_llm | json_parser
 
 categories = '''เรียกรับสินบน ให้/ขอให้ หรือรับว่าจะให้ทรัพย์สินหรือประโยชน์อื่นใดแก่เจ้าหน้าที่ของรัฐ
 จัดซื้อจัดจ้าง
@@ -35,18 +71,22 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-ON_PREM = True
 if ON_PREM:
     llm = ChatOllama(model="gemma2")
 else:
     llm = ChatOpenAI(model="gpt-4o-mini")
 
-parser = StrOutputParser()
+class StrOutputParserWithStrip(StrOutputParser):
+    def parse(self, text):
+        return super().parse(text).strip()
+parser = StrOutputParserWithStrip()
+
+categorizer_chain = prompt | llm | parser
 
 app = FastAPI(
-    title="LangChain Server",
+    title="NACC LLM Server",
     version="1.0",
-    description="A simple api server using Langchain's Runnable interfaces",
+    description="This is a server for NACC LLM",
 )
 
 # Set all CORS enabled origins
@@ -61,7 +101,13 @@ app.add_middleware(
 
 add_routes(
     app,
-    prompt | llm | parser,
+    summarizer_chain,
+    path="/summarizer",
+)
+
+add_routes(
+    app,
+    categorizer_chain,
     path="/categorizer",
 )
 
